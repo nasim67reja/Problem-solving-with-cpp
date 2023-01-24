fbd783a7d7fcff7577025744b0b13465ee31e538
Nasim Reja <nasimreja67@gmail.com>
2023-01-24 10:51:59 +0600

    some leetcode sorting problem added

--- Sorting/js/sortingType.js
+++ Sorting/js/sortingType.js
@@ -31,4 +31,39 @@ var containsDuplicate = function (nums) {
 };
 
 const nums = [1, 2, 3, 1];
-console.log(containsDuplicate(nums));
+// console.log(containsDuplicate(nums));
+
+// 2160. Minimum Sum of Four Digit Number After Splitting Digits
+
+var minimumSum = function (num) {
+  let arr = [];
+
+  for (let i = 0; i < 4; i++) {
+    arr[i] = num % 10;
+    num = Math.floor(num / 10);
+  }
+  arr.sort((a, b) => a - b);
+  let str1 = `${arr[0]}${arr[3]}`;
+  let str2 = `${arr[1]}${arr[2]}`;
+
+  return Number(str1) + Number(str2);
+};
+
+// console.log(minimumSum(4009));
+
+// 1365. How Many Numbers Are Smaller Than the Current Number
+
+var smallerNumbersThanCurrent = function (nums) {
+  let output = [];
+  for (let i = 0; i < nums.length; i++) {
+    let count = 0;
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[i] > nums[j]) count++;
+    }
+    output.push(count);
+  }
+  return output;
+};
+
+smallerNumbersThanCurrent([8, 1, 2, 2, 3]);
+smallerNumbersThanCurrent([6, 5, 4, 8]);
--- Sorting/sortingType.cpp
+++ Sorting/sortingType.cpp
@@ -17,9 +17,9 @@ vector<int> reverse(vector<int> v)
     return v;
 }
 
-void printVector(vector<int> v)
+void printVector(auto v)
 {
-    for (int val : v)
+    for (auto val : v)
         cout << val << " ";
     cout << endl;
 }
@@ -53,20 +53,63 @@ bool containsDuplicate(vector<int> &nums)
     return false;
 }
 
+// 2160. Minimum Sum of Four Digit Number After Splitting Digits
+
+int minimumSum(int num)
+{
+    vector<string> v;
+    for (int i = 0; i < 4; i++)
+    {
+        string s = to_string(num % 10);
+        v.push_back(s);
+        num = (num / 10);
+    }
+    sort(v.begin(), v.end());
+    string s = v[0] + v[3];
+    string s2 = v[1] + v[2];
+
+    return stoi(s) + stoi(s2);
+
+    // alternative
+    // int a[4] = {n / 1000, n / 100 % 10, n / 10 % 10, n % 10};
+    // sort(a, a + 4);
+    // return a[0] * 10 + a[1] * 10 + a[2] + a[3];
+}
+
+// Leetcode 1365. How Many Numbers Are Smaller Than the Current Number
+
+vector<int> smallerNumbersThanCurrent(vector<int> &nums)
+{
+
+    sort(nums.begin(), nums.end());
+    vector<int> v;
+
+    for (int val : nums)
+    {
+        auto low = lower_bound(nums.begin(), nums.end(), val);
+        cout << low - nums.begin() << "";
+    }
+    return nums;
+}
+
 int main()
 {
-    vector<int> v;
-    v.push_back(4);
-    v.push_back(7);
-    v.push_back(2);
-    v.push_back(4);
-    v.push_back(21);
+    vector<int> vt{8, 1, 2, 2, 3};
+    smallerNumbersThanCurrent(vt);
+
+    // cout << minimumSum(4009);
+    // vector<int> v;
+    // v.push_back(4);
+    // v.push_back(7);
+    // v.push_back(2);
+    // v.push_back(4);
+    // v.push_back(21);
 
     // vector<int> rv = reverse(v);
     // cout << "Before Reversing ";
     // printVector(v);
     // cout << "After Reversing ";
     // printVector(rv);
-    cout << containsDuplicate(v) << endl;
+    // cout << containsDuplicate(v) << endl;
     return 0;
 }
